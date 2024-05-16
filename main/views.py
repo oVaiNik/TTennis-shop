b711b4403c15f3ed22ace9dc85011b6eba0ce65b
TTennis-shop <shadowvova2299@gmail.com>
2024-05-16 21:07:37 +0900

    goods tags

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from goods.models import Categories
 
 def index(request):
-    categories = Categories.objects.all()
+    
     context = {
         'title': 'Home - Главное меню',
         'content': 'Магазин настольного тенниса',
-        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
