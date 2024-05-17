061abbe48f8350658738fbfa3c858f5608d6cbb9
TTennis-shop <shadowvova2299@gmail.com>
2024-05-17 23:55:13 +0900

    get page

--- goods/views.py
+++ goods/views.py
@@ -6,6 +6,7 @@ from goods.models import Products
 
 def catalog(request, category_slug):
 
+    page = request.GET.get('page', 1)
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
@@ -13,7 +14,7 @@ def catalog(request, category_slug):
         
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
     
     context = {
         "title": "Home - Каталог",
