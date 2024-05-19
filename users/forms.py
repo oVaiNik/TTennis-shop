45f48728e83be3866619a4ea29e8b4f96f0bef69
TTennis-shop <shadowvova2299@gmail.com>
2024-05-20 02:16:05 +0900

    Register

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -11,20 +11,37 @@ class UserLoginForm(AuthenticationForm):
         model = User
         field = ['username', 'password']
 
-
-    # username = forms.CharField(
-    #     label= 'Имя',
-    #     widget=forms.TextInput(attrs={
-    #         "autofocus": True,
-    #         'class': 'form-control',
-    #         'placegolder': 'Введите  ваше имя пользователя'
-    #         })
-    #     )
-    # password = forms.CharField(
-    #     label = 'Пароль',
-    #     widget=forms.PasswordInput(attrs={
-    #         "autocomplete": "current-password",
-    #         'class': 'form-control',
-    #         "placeholder": "Введите ваш пароль"
-    #         }),
-    # )
+class UserRegistrationForm():
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+            
+# username = forms.CharField(
+#     label= 'Имя',
+#     widget=forms.TextInput(attrs={
+#         "autofocus": True,
+#         'class': 'form-control',
+#         'placegolder': 'Введите  ваше имя пользователя'
+#         })
+#     )
+# password = forms.CharField(
+#     label = 'Пароль',
+#     widget=forms.PasswordInput(attrs={
+#         "autocomplete": "current-password",
+#         'class': 'form-control',
+#         "placeholder": "Введите ваш пароль"
+#         }),
+# )
