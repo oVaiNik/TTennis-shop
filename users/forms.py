7ab49f97f82d7b5e19c1eddfa60f211cf17d7b68
TTennis-shop <shadowvova2299@gmail.com>
2024-05-21 00:00:14 +0900

    Profile

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
+from dataclasses import field
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -11,37 +12,38 @@ class UserLoginForm(AuthenticationForm):
         model = User
         field = ['username', 'password']
 
-class UserRegistrationForm():
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            )
+        
     first_name = forms.CharField()
     last_name = forms.CharField()
     username = forms.CharField()
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = (
-            'first_name',
-            'last_name',
-            'username',
-            'email',
-            'password1',
-            'password2',
-        )
-            
-# username = forms.CharField(
-#     label= 'Имя',
-#     widget=forms.TextInput(attrs={
-#         "autofocus": True,
-#         'class': 'form-control',
-#         'placegolder': 'Введите  ваше имя пользователя'
-#         })
-#     )
-# password = forms.CharField(
-#     label = 'Пароль',
-#     widget=forms.PasswordInput(attrs={
-#         "autocomplete": "current-password",
-#         'class': 'form-control',
-#         "placeholder": "Введите ваш пароль"
-#         }),
-# )
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            )
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 def login(request):
     if request.method == 'POST':
@@ -14,6 +15,9 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"Вы вошли как, {username}")
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -33,6 +37,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"Вы успешно зарегистрированы как - {user.username}")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -44,15 +49,27 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Профиль успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+    
     context = {
-        'title': 'Home - Профиль'
+        'title': 'Home - Профиль',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
 
 
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")    
     auth.logout(request)
     return redirect(reverse('main:index'))
