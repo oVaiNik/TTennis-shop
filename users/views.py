2664fd2e1e5a2eb584c8a79921e73942b1d28c8c
TTennis-shop <shadowvova2299@gmail.com>
2024-05-28 11:40:15 +0900

    asd

--- users/views.py
+++ users/views.py
@@ -16,9 +16,12 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"Вы вошли как, {username}")
+
                 redirect_page = request.POST.get('next', None)
+
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
+                
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
