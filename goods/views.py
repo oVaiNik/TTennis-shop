492e60f5321e81b146937296fe07d37d49da353e
oVaiNik <shadowvova2299@gmail.com>
2025-05-28 01:10:15 +0900

    asd

--- goods/views.py
+++ goods/views.py
@@ -1,37 +1,38 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import render
+from django.db.models import Q
 
 from goods.models import Products
 from goods.utils import q_search
 
 
 def catalog(request, category_slug=None):
-
     page = request.GET.get('page', 1)
-
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
-    
     query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     elif query:
         goods = q_search(query)
+        if isinstance(goods, list):
+            goods = Products.objects.filter(id__in=[item.id for item in goods])
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug = category_slug))
+        goods = Products.objects.filter(category__slug=category_slug)
+        if not goods.exists():
+            raise Http404("Категория не найдена")
 
     if on_sale:
         goods = goods.filter(discount__gt=0)
     if order_by and order_by != "default":
         goods = goods.order_by(order_by)
-        
 
-    paginator = Paginator(goods, 3)
+    paginator = Paginator(goods, 9)
     current_page = paginator.page(int(page))
     
     context = {
-        "title": "Home - Каталог",
+        "title": "Free style - Каталог",
         "goods": current_page,
         "slug_url": category_slug,
     }
@@ -40,9 +41,7 @@ def catalog(request, category_slug=None):
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
-
     context = {
         'product': product,
     }
-
     return render(request, "goods/product.html", context=context)
--- main/views.py
+++ main/views.py
@@ -1,21 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories, Products
+from django.utils.safestring import mark_safe
+import random
 
-from goods.models import Categories
 
-def index(request):
-    
+def index(request, all_products = None):
+    all_products = list(Products.objects.all())
+
+    random.shuffle(all_products)
+    random_products = all_products[:9]
     context = {
         'title': 'Free style - Главное меню',
-        'content': 'Магазин настольного тенниса',
+        'content': 'Магазин мужской одежды и аксессуаров',
+        'products': random_products,
     }
-
     return render(request, 'main/index.html', context)
+
+
+
 
 def about(request):
     context = {
         'title': 'Free style - О нас',
-        'content': 'О нас',
-        'text_on_page': "Наша компания появилась в 90-х годах, когда в мире настольного тенниса царили другие правила, мы наблюдали за ходом истории в нашем виде спорта, и параллельно успешно развивались. Сегодня наш каталог товаров для настольного тенниса состоит из тысяч единиц разнообразных товаров, и постоянно пополняется. Здесь можно купить накладки, основания, мячи, клеи и средства по уходу за теннисной ракеткой, а также теннисную форму и обувь, различные сопутствующие товары и аксессуары. Мы работаем с самыми известными брендами: Andro, Butterfly, Yasaka, TSP, Victas, Nittaku, Xiom, Joola, Gewo, Friendship, Dr.Neubauer, Tibhar, Donic, DHS, Stiga и многие другие. Если вы уже обращались в наш интернет-магазин, то с легкостью сможете найти интересующий вас товар, используя удобные и понятные фильтры. Также советуем использовать поиск по наименованию товаров, который ускорит процесс вашего выбора."
+        'content': mark_safe('О нас'),
+        'text_on_page': mark_safe(
+            "Free style – это магазин модной одежды, где каждый найдет что-то по душе. " \
+            "Мы предлагаем стильные, удобные и качественные вещи, которые помогут вам выразить свою индивидуальность.<br><br>" \
+            "<strong>Почему выбирают нас?</strong>" \
+            "<ul>" \
+            "<li><strong>Актуальные тренды </strong>– следим за модными тенденциями и предлагаем только лучшее.</li>" \
+            "<li><strong>Качество и комфорт </strong>– работаем с проверенными поставщиками, чтобы вы получали долговечные и приятные к носке вещи.</li>" \
+            "<li><strong>Доступные цены </strong>– мода должна быть для всех, поэтому у нас есть варианты на любой бюджет.</li>" \
+            "</ul><br>" \
+        )
     }
     return render(request, 'main/about.html', context)
